fdf7cadcd6625b40d21dac50fa41bfda48f1b183
shaikakhila26 <akhilashaik2605@gmail.com>
2025-08-16 10:57:59 +0530

    removed supabaseClient.js from gitignore as it doesnot contain secrets

---
+++ supabaseClient.js
@@ -0,0 +1,22 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
+
+
+
+
+
+
+
+/*
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+*/
